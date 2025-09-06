a0ca8bafeb546f0baf0caf2c02cca8fba4b74d51
Xevroniel <raiyansaifjt@gmail.com>
2025-09-06 14:44:40 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter, Plus_Jakarta_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
